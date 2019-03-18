20f158038deb491df89e874bf6a3ac719eb92d8c
Junghwan Park <reserve.dev@gmail.com>
2019-03-18 21:37:25 +0900

    Package metadata 추가

--- python/PyKomoran/__version__.py
+++ python/PyKomoran/__version__.py
@@ -6,10 +6,27 @@
 #          `---'
 
 __title__ = 'PyKomoran'
-__description__ = 'Python wrapper for KOMORAN, KOrean MORphical ANalyzer.'
+__description__ = 'PyKomoran is Python wrapper for KOMORAN, KOrean MORphical ANalyzer.'
 __url__ = 'https://docs.komoran.kr'
-__version__ = '0.1.0a1'
+__version__ = '0.1.0'
 __author__ = 'Junghwan Park'
 __author_email__ = 'reserve.dev@gmail.com'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2019 Junghwan Park'
+__classifiers__ = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: Korean',
+    'Natural Language :: English',
+    'Programming Language :: Java',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Text Processing :: Linguistic'
+],
+__download_url__ = 'https://github.com/komoran/PyKOMORAN',
+__keywords__ = 'KOMORAN, PyKomoran, KOrean MORphical ANalyzer, Morph Analyzer',
