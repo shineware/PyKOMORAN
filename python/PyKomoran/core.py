e6d2cd08c58a4eac3e58d9209ade5daa7232351e
Junghwan Park <reserve.dev@gmail.com>
2019-03-18 21:35:55 +0900

    PyKomoran v0.1 개선

--- python/PyKomoran/core.py
+++ python/PyKomoran/core.py
@@ -8,47 +8,72 @@ __all__ = ['Komoran']
 
 
 class KomoranError(Exception):
+    """
+    Handle exceptions occur in Komoran Class
+    """
+
     def __init__(self, args=None, cause=None):
         super(KomoranError, self).__init__(args)
         self.cause = cause
 
 
 class Komoran:
-    def __init__(self, model_path="./models_full"):
-        self.base_path = os.path.dirname(os.path.realpath(__file__))
-        self.model_path = os.path.join(self.base_path, model_path)
+    """
+    Komoran Wrapper class
+    """
 
-        assert os.path.exists(self.model_path)
+    def __init__(self, model_path="./models_full"):
+        self._base_path = os.path.dirname(os.path.realpath(__file__))
+        self._model_path = os.path.abspath(os.path.join(self._base_path, model_path))
+
+        assert os.path.exists(self._model_path)
 
         jvm.init_jvm()
-
         self._komoran = jvm.get_jvm().kr.co.shineware.nlp.pykomoran.KomoranEntryPoint()
-        self._komoran.init(model_path)
 
+        self._komoran.init(self._model_path)
+        if not self._komoran.isInitialized():
+            raise KomoranError("Komoran is NOT initialized!")
+
+    def _validate_initialized(func):
+        def wrapper(self, *args, **kwargs):
+            if not self._komoran.isInitialized():
+                raise KomoranError("Komoran is NOT initialized!")
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    @_validate_initialized
     def set_user_dic(self, dic_path):
         self._komoran.setUserDic(dic_path)
 
+    @_validate_initialized
     def set_fw_dic(self, dic_path):
         self._komoran.setFWDic(dic_path)
 
-    def analyze(self, sentence):
+    @_validate_initialized
+    def get_nouns(self, sentence):
         self._komoran.analyze(sentence)
+        return list(self._komoran.getNouns())
 
-    def get_nouns(self):
-        return self._komoran.getNouns()
+    @_validate_initialized
+    def get_morphes_by_tags(self, sentence, tag_list=list()):
+        self._komoran.analyze(sentence)
+        return list(self._komoran.getMorphesByTags(tag_list))
 
-    def get_morphes_by_tags(self, tag_list):
-        return self._komoran.getMorphesByTags(tag_list)
+    @_validate_initialized
+    def get_plain_text(self, sentence):
+        self._komoran.analyze(sentence)
+        return str(self._komoran.getPlainText())
 
-    def get_plaint_text(self):
-        return self._komoran.getPlainText()
-
-    def get_token_list(self, flatten=True):
+    @_validate_initialized
+    def get_token_list(self, sentence, flatten=True, use_pos_name=False):
+        self._komoran.analyze(sentence)
         token_dict_array = self._komoran.getTokenList()
-        tokens = [Token(token) for token in token_dict_array]
+        tokens = [Token(token, use_pos_name=use_pos_name) for token in token_dict_array]
 
         if flatten:
-            return tokens
+            return list(tokens)
 
         tokens_separated = list()
         eojeol = list()
@@ -61,21 +86,24 @@ class Komoran:
 
         return tokens_separated
 
-    def get_list(self):
+    @_validate_initialized
+    def get_list(self, sentence):
+        self._komoran.analyze(sentence)
         pair_dict_array = self._komoran.getList()
         pair_array = [Pair(pair) for pair in pair_dict_array]
 
-        return pair_array
+        return list(pair_array)
 
 
 if __name__ == '__main__':
     komoran = Komoran()
+    str_to_analyze = "① 대한민국은 민주공화국이다. ② 대한민국의 주권은 국민에게 있고, 모든 권력은 국민으로부터 나온다."
 
-    print(komoran)
-    print(komoran.analyze("① 대한민국은 민주공화국이다. ② 대한민국의 주권은 국민에게 있고, 모든 권력은 국민으로부터 나온다."))
-    print(komoran.get_nouns())
-    print(komoran.get_morphes_by_tags(["NNG", "EF", "SF"]))
-    print(komoran.get_plaint_text())
-    print(komoran.get_token_list())
-    print(komoran.get_token_list(flatten=False))
-    print(komoran.get_list())
+    print(komoran.get_nouns(str_to_analyze))
+    print(komoran.get_morphes_by_tags(str_to_analyze, tag_list=['NNP', "NNG", "SF", "NN", "123"]))
+    print(komoran.get_morphes_by_tags(str_to_analyze))
+    print(komoran.get_plain_text(str_to_analyze))
+    print(komoran.get_token_list(str_to_analyze))
+    print(komoran.get_token_list(str_to_analyze, flatten=False))
+    print(komoran.get_token_list(str_to_analyze, flatten=False, use_pos_name=True))
+    print(komoran.get_list(str_to_analyze))
--- python/PyKomoran/jvm.py
+++ python/PyKomoran/jvm.py
@@ -5,12 +5,14 @@ from py4j.java_gateway import GatewayParameters
 from py4j.java_gateway import JavaGateway
 from py4j.java_gateway import launch_gateway
 
+__all__ = ['jvm_gateway', 'init_jvm', 'get_jvm']
+
 jvm_gateway = None
 
 
-def init_jvm(jar_path="./lib", max_heap=1024):
+def init_jvm(jar_path="./libs", max_heap=1024):
     base_path = os.path.dirname(os.path.realpath(__file__))
-    jar_path = os.path.join(base_path, jar_path)
+    jar_path = os.path.abspath(os.path.join(base_path, jar_path))
 
     assert os.path.exists(jar_path)
 
@@ -22,25 +24,25 @@ def init_jvm(jar_path="./lib", max_heap=1024):
     libraries = [
         '{0}{1}KOMORAN-3.3.4.jar',
         '{0}{1}KOMORANEntryPoint-0.1.0.jar',
-        '{0}{1}*'
+        # '{0}{1}*'
     ]
 
     classpath = os.pathsep.join([lib.format(jar_path, os.sep) for lib in libraries])
-    py4j_path = "{}/py4j0.10.8.1.jar".format(jar_path)
+    py4j_path = "{0}{1}py4j0.10.8.1.jar".format(jar_path, os.sep)
 
     port = launch_gateway(jarpath=py4j_path,
                           classpath=classpath,
                           javaopts=['-Dfile.encoding=UTF8', '-ea', '-Xmx{}m'.format(max_heap)],
                           die_on_exit=True)
 
-    logging.debug("initializing JVM... ", end="")
-
+    logging.debug("initializing JVM... ")
     try:
         jvm_gateway = JavaGateway(gateway_parameters=GatewayParameters(port=port, auto_convert=True))
+        # # for debugging with Java-side
+        # jvm_gateway = JavaGateway(gateway_parameters=GatewayParameters(port=25335, auto_convert=True))
     except Exception as e:
         jvm_gateway = None
         logging.debug("fail")
-
     logging.debug("success")
 
     return jvm_gateway.jvm
